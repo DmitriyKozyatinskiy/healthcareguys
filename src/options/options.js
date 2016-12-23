75c61ffff20fc6fb07bc218ed60328535c226e07
DmitriyKozyatinskiy <dmitriy.kozyatinskiy@gmail.com>
2016-12-23 07:21:17 +0200

    Update options page

--- src/options/options.js
+++ src/options/options.js
@@ -1,5 +1,17 @@
 import $ from 'jquery';
 import './options.scss';
+import changeLog from './changelog.html';
+
+const $currentVersionNumber = $('#js-current-version');
+const $changeLogContainer = $('#js-changelog-container');
 
 const manifest = chrome.runtime.getManifest();
-$('#js-current-version').html(manifest.version);
+$currentVersionNumber.html(manifest.version);
+
+
+function showChangeLog(event) {
+  event.preventDefault();
+  $changeLogContainer.html(changeLog).removeClass('hidden');
+}
+
+$(document).on('click', '#js-changelog-button', event => showChangeLog(event));
